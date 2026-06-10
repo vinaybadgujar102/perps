8f02aa2165b37d7f63099a9f3339800bd72cad44
Vinay Badgujar <vinaybadgujar8@gmail.com>
2026-06-11 01:25:59 +0530

    add tanstack form to this login form

--- apps/tanstack-frontend/src/routes/login.tsx
+++ apps/tanstack-frontend/src/routes/login.tsx
@@ -3,20 +3,31 @@ import { Input } from "#/components/ui/input";
 import { createFileRoute } from "@tanstack/react-router";
 import { MoveRight } from "lucide-react";
 import { useForm } from "@tanstack/react-form";
-import {  } from "@repo/sharedtypes"
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from "@/components/ui/field";
+import { loginSchema } from "@repo/sharedtypes";
 export const Route = createFileRoute("/login")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  const form = useForm({
+  const loginForm = useForm({
     defaultValues: {
-      
+      email: "",
+      password: "",
     },
     validators: {
-      onSubmit: schema
-    }
+      onSubmit: loginSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log("form submitted", value);
+    },
   });
+
   return (
     <div className="flex h-screen items-center justify-center m-8">
       {/* Left Part */}
@@ -47,25 +58,96 @@ function RouteComponent() {
       <section className="flex-1 border-l-foreground-muted border-l">
         {/* Form */}
         <div className="p-8 flex flex-col gap-4 grow-0">
-          <div className="flex flex-col gap-4">
-            <div className="flex flex-col gap-1.5">
-              <p className="text-neutral-400 tracking-wider text-xs">EMAIL</p>
-              <Input
-                className="bg-foreground h-12 placeholder:text-gray-400"
-                placeholder="USER@PERPS.IO"
+          <form
+            id="login-form"
+            onSubmit={(e) => {
+              e.preventDefault();
+              loginForm.handleSubmit(e);
+            }}
+          >
+            <FieldGroup className="flex flex-col gap-4">
+              <loginForm.Field
+                name="email"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field
+                      className="flex flex-col gap-1.5"
+                      data-invalid={isInvalid}
+                    >
+                      <FieldLabel
+                        htmlFor={field.name}
+                        className="text-neutral-400 tracking-wider text-xs"
+                      >
+                        EMAIL
+                      </FieldLabel>
+                      <Input
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        className="bg-foreground h-12 placeholder:text-gray-400"
+                        onBlur={field.handleBlur}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        aria-invalid={isInvalid}
+                        placeholder="USER@PERPS.IO"
+                        autoComplete="off"
+                      />
+                      {isInvalid && (
+                        <FieldError
+                          className="text-accent"
+                          errors={field.state.meta.errors}
+                        />
+                      )}
+                    </Field>
+                  );
+                }}
               />
-            </div>
-            <div className="flex flex-col gap-1.5">
-              <p className="text-neutral-400 tracking-wider text-xs">
-                PASSWORD
-              </p>
-              <Input
-                className="bg-foreground h-12 placeholder:text-gray-400"
-                placeholder="USER@PERPS.IO"
+
+              <loginForm.Field
+                name="password"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched && !field.state.meta.isValid;
+                  return (
+                    <Field
+                      className="flex flex-col gap-1.5"
+                      data-invalid={isInvalid}
+                    >
+                      <FieldLabel
+                        htmlFor={field.name}
+                        className="text-neutral-400 tracking-wider text-xs"
+                      >
+                        PASSWORD
+                      </FieldLabel>
+                      <Input
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        onChange={(e) => field.handleChange(e.target.value)}
+                        className="bg-foreground h-12 placeholder:text-gray-400"
+                        onBlur={field.handleBlur}
+                        aria-invalid={isInvalid}
+                        placeholder="USER@PERPS.IO"
+                        autoComplete="off"
+                      />
+                      {isInvalid && (
+                        <FieldError
+                          className="text-accent"
+                          errors={field.state.meta.errors}
+                        />
+                      )}
+                    </Field>
+                  );
+                }}
               />
-            </div>
-          </div>
-          <Button className="pl-0! text-xl bg-transparent text-foreground tracking-widest font-extrabold w-fit cursor-pointer">
+            </FieldGroup>
+          </form>
+          <Button
+            type="submit"
+            form="login-form"
+            className="pl-0! text-xl bg-transparent text-foreground tracking-widest font-extrabold w-fit cursor-pointer"
+          >
             LOGIN
             <MoveRight strokeWidth={4} size={5} />
           </Button>
