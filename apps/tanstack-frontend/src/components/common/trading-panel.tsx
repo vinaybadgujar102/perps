91907724ab72bbbbf06cc55478cc8ffe761200d5
Vinay Badgujar <vinaybadgujar8@gmail.com>
2026-06-11 16:53:56 +0530

    add onsubmit with dummy flow

--- apps/tanstack-frontend/src/components/common/trading-panel.tsx
+++ apps/tanstack-frontend/src/components/common/trading-panel.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { useForm } from "@tanstack/react-form";
 import { createOrderSchema, ORDER_TYPE, SIDE } from "@repo/sharedtypes";
 import { Button } from "#/components/ui/button";
@@ -8,18 +9,24 @@ import {
   FieldLabel,
 } from "#/components/ui/field";
 import { Input } from "#/components/ui/input";
+import { cn } from "#/lib/utils";
 
 export function TradingPanel() {
-  const limitOrderForm = useForm({
+  const [orderType, setOrderType] = useState(ORDER_TYPE.LIMIT_ORDER);
+
+  const orderForm = useForm({
     defaultValues: {
       price: 0,
       market: "BTC",
       side: SIDE.LONG,
       qty: 0,
-      orderType: ORDER_TYPE.LIMIT_ORDER,
+      orderType: orderType,
     },
     validators: {
       onSubmit: createOrderSchema,
+    },
+    onSubmit: async ({ value }) => {
+      console.log(value);
     },
   });
 
@@ -27,10 +34,34 @@ export function TradingPanel() {
     <div className="flex-1 p-4 border border-border flex flex-col justify-between">
       <div className="flex flex-col gap-4">
         <div>
-          <Button className="bg-transparent text-input-label hover:text-foreground font-semibold tracking-wider cursor-pointer">
+          <Button
+            type="button"
+            className={cn(
+              "bg-transparent hover:text-foreground font-semibold tracking-wider cursor-pointer rounded-none underline-offset-4",
+              orderType === ORDER_TYPE.LIMIT_ORDER
+                ? "text-foreground underline decoration-accent underline-offset-8"
+                : "text-input-label",
+            )}
+            onClick={() => {
+              setOrderType(ORDER_TYPE.LIMIT_ORDER);
+              orderForm.setFieldValue("orderType", ORDER_TYPE.LIMIT_ORDER);
+            }}
+          >
             LIMIT
           </Button>
-          <Button className="bg-transparent text-input-label hover:text-foreground font-semibold tracking-wider cursor-pointer">
+          <Button
+            type="button"
+            className={cn(
+              "bg-transparent hover:text-foreground font-semibold tracking-wider cursor-pointer rounded-none underline-offset-4",
+              orderType === ORDER_TYPE.MARKET_ORDER
+                ? "text-foreground underline decoration-accent underline-offset-8"
+                : "text-input-label",
+            )}
+            onClick={() => {
+              setOrderType(ORDER_TYPE.MARKET_ORDER);
+              orderForm.setFieldValue("orderType", ORDER_TYPE.MARKET_ORDER);
+            }}
+          >
             MARKET
           </Button>
         </div>
@@ -38,48 +69,50 @@ export function TradingPanel() {
           id="order-form"
           onSubmit={(e) => {
             e.preventDefault();
-            limitOrderForm.handleSubmit(e);
+            orderForm.handleSubmit(e);
           }}
         >
           <FieldGroup className="flex flex-col gap-6 border-b border-b-border p-4 pb-8">
-            <limitOrderForm.Field
-              name="price"
-              children={(field) => {
-                const isInvalid =
-                  field.state.meta.isTouched || !field.state.meta.isValid;
-                return (
-                  <Field className="flex flex-col gap-1.5">
-                    <FieldLabel
-                      htmlFor={field.name}
-                      className="text-input-label tracking-widest font-semibold"
-                    >
-                      PRICE
-                    </FieldLabel>
-                    <Input
-                      className="bg-surface-container h-10 border-border"
-                      id={field.name}
-                      name={field.name}
-                      value={field.state.value}
-                      type="number"
-                      min={0}
-                      onChange={(e) =>
-                        field.handleChange(Number(e.target.value))
-                      }
-                      aria-invalid={isInvalid}
-                      autoComplete="off"
-                    />
-                    {isInvalid && (
-                      <FieldError
-                        className="text-accent"
-                        errors={field.state.meta.errors}
+            {orderType === ORDER_TYPE.LIMIT_ORDER && (
+              <orderForm.Field
+                name="price"
+                children={(field) => {
+                  const isInvalid =
+                    field.state.meta.isTouched || !field.state.meta.isValid;
+                  return (
+                    <Field className="flex flex-col gap-1.5">
+                      <FieldLabel
+                        htmlFor={field.name}
+                        className="text-input-label tracking-widest font-semibold"
+                      >
+                        PRICE
+                      </FieldLabel>
+                      <Input
+                        className="bg-surface-container h-10 border-border"
+                        id={field.name}
+                        name={field.name}
+                        value={field.state.value}
+                        type="number"
+                        min={0}
+                        onChange={(e) =>
+                          field.handleChange(Number(e.target.value))
+                        }
+                        aria-invalid={isInvalid}
+                        autoComplete="off"
                       />
-                    )}
-                  </Field>
-                );
-              }}
-            />
+                      {isInvalid && (
+                        <FieldError
+                          className="text-accent"
+                          errors={field.state.meta.errors}
+                        />
+                      )}
+                    </Field>
+                  );
+                }}
+              />
+            )}
 
-            <limitOrderForm.Field
+            <orderForm.Field
               name="qty"
               children={(field) => {
                 const isInvalid =
@@ -134,13 +167,23 @@ export function TradingPanel() {
         </div>
       </div>
 
-      <div className="flex mb-4 gap-2  text-xs tracking-widest">
-        {/* Buy Sell button */}
-
-        <Button className="bg-trading-green font-bold  tracking-widest flex-1 text-black">
+      <div className="flex mb-4 gap-2 text-xs tracking-widest">
+        <Button
+          type="submit"
+          form="order-form"
+          className="bg-trading-green font-bold tracking-widest flex-1 text-black"
+          onClick={() => orderForm.setFieldValue("side", SIDE.LONG)}
+        >
           BUY / LONG
         </Button>
-        <Button className="bg-accent flex-1 font-bold">SELL / SHORT</Button>
+        <Button
+          type="submit"
+          form="order-form"
+          className="bg-accent flex-1 font-bold"
+          onClick={() => orderForm.setFieldValue("side", SIDE.SHORT)}
+        >
+          SELL / SHORT
+        </Button>
       </div>
     </div>
   );
