b097443f6a6ac4149e05cfca05ea089a86e3baef
Vinay Badgujar <vinaybadgujar8@gmail.com>
2026-06-15 01:24:35 +0530

    feat: add snapshot creation logic

--- apps/trade-engine/src/index.ts
+++ apps/trade-engine/src/index.ts
@@ -1,6 +1,20 @@
 import { eventSchema, QUEUES } from "@repo/sharedtypes";
 import { createClient } from "redis";
 import { handleIncomingEvents } from "./handlers";
+import { POSITIONS, USERMANAGER } from "./inMemoryStates";
+
+export async function writeSnapshot() {
+  let count = 0;
+  count++;
+  const snapshot = {
+    count: count,
+    users: USERMANAGER.getAllUsers(),
+    positions: Array.from(POSITIONS.entries()),
+    lastProcessedId: lastId,
+  };
+
+  await Bun.write("snapshot.json", JSON.stringify(snapshot));
+}
 
 const subscriberRedis = await createClient().connect();
 let lastId = "0";
@@ -9,7 +23,7 @@ while (true) {
   try {
     const res = await subscriberRedis.xRead(
       { key: QUEUES.SEND_QUEUE, id: lastId },
-      { BLOCK: 0, COUNT: 1 },
+      { BLOCK: 1000, COUNT: 1 },
     );
     if (!res || !Array.isArray(res)) continue;
     const message = res[0]?.messages?.[0];
